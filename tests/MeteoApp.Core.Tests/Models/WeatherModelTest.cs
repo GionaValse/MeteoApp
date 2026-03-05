cbb8f70918ebd742e136f922f8c445f2c50525ff
Valsecchi Giona <giona.valsecchi@student.supsi.ch>
2026-03-05 14:31:42 +0100

    Core + Test added

---
+++ tests/MeteoApp.Core.Tests/Models/WeatherModelTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteoApp.Core.Models
+{
+    internal class WeatherModelTest
+    {
+    }
+}
---
+++ tests/MeteoApp.Core.Tests/Services/WeatherServiceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteoApp.Core.Services
+{
+    internal class WeatherServiceTest
+    {
+    }
+}
